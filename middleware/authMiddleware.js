3658fff472798bbe04d34def533bd21d98be0770
Nikola Vukovic <nikolav54@gmail.com>
2021-06-29 12:32:58 +0200

    Added admin dashboard middleware and admin checking

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const { reset } = require("nodemon");
 const admin = require("../firebase");
+const User = require("../models/userModel");
 
 exports.authCheck = async (req, res, next) => {
   console.log(req.headers);
@@ -14,5 +15,19 @@ exports.authCheck = async (req, res, next) => {
     res.status(401).json({
       error: "Invalid or expired token! Error: " + err.message,
     });
+  }
+};
+
+exports.adminCheck = async (req, res, next) => {
+  const { email } = req.user;
+
+  const adminUser = await User.findOne({ email }).exec();
+
+  if (adminUser.role !== "admin") {
+    res.status(403).json({
+      err: "Admin resource. Access denied!",
+    });
+  } else {
+    next();
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { createOrUpdateUser } = require("../controllers/authController");
-const { authCheck } = require("../middleware/authMiddleware");
+const {
+  createOrUpdateUser,
+  currentUser,
+} = require("../controllers/authController");
+const { authCheck, adminCheck } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+router.post("/current-user", authCheck, currentUser);
+router.post("/current-admin", authCheck, adminCheck, currentUser);
 
 module.exports = router;
